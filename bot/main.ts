6d648feb8aaa299eb9cdea14b13628f26453c5ba
vetnem1lk <vetnem1lk@mail.ru>
2024-04-02 22:05:17 +0700

    initial

---
+++ bot/main.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { bot } from './bot';
+import { getWallets } from './ton-connect/wallets';
+import QRCode from 'qrcode';
+import { getConnector } from './ton-connect/connector';
+
+bot.onText(/\/connect/, async msg => {
+    const chatId = msg.chat.id;
+    const wallets = await getWallets();
+
+    const connector = getConnector(chatId);
+
+    connector.onStatusChange(wallet => {
+        if (wallet) {
+            bot.sendMessage(chatId, `${wallet.device.appName} wallet connected!`);
+        }
+    });
+
+    const tonkeeper = wallets.find(wallet => wallet.appName === 'tonkeeper')!;
+
+    const link = connector.connect({
+        bridgeUrl: tonkeeper.bridgeUrl,
+        universalLink: tonkeeper.universalLink
+    });
+    const image = await QRCode.toBuffer(link);
+
+    await bot.sendPhoto(chatId, image);
+});
---
+++ bot/ton-connect/utils.ts
@@ -0,0 +1,28 @@
+// src/utils.ts
+import { encodeTelegramUrlParameters, isTelegramUrl } from '@tonconnect/sdk';
+
+export const AT_WALLET_APP_NAME = 'telegram-wallet';
+
+// ... other code
+export function addTGReturnStrategy(link: string, strategy: string): string {
+    const parsed = new URL(link);
+    parsed.searchParams.append('ret', strategy);
+    link = parsed.toString();
+
+    const lastParam = link.slice(link.lastIndexOf('&') + 1);
+    return link.slice(0, link.lastIndexOf('&')) + '-' + encodeTelegramUrlParameters(lastParam);
+}
+
+export function convertDeeplinkToUniversalLink(link: string, walletUniversalLink: string): string {
+    const search = new URL(link).search;
+    const url = new URL(walletUniversalLink);
+
+    if (isTelegramUrl(walletUniversalLink)) {
+        const startattach = 'tonconnect-' + encodeTelegramUrlParameters(search.slice(1));
+        url.searchParams.append('startattach', startattach);
+    } else {
+        url.search = search;
+    }
+
+    return url.toString();
+}
